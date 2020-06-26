965fd16d008cf48657d9200f15042780f33afeb5
stasi815 <anastasia.gallardo@students.makeschool.com>
2020-06-26 14:16:46 -0700

    [add] defined a store, implemented Redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from "./reducers"
 import './App.css';
 import Password from './password';
 
+const store = createStore(reducers)
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Password />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <Password />
+        </div>
+      </Provider>
     );
   }
 }
